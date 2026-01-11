a4ca1f2c7246d1f24ac6c80736e512915d9552c8
Asilbek <Leonlloyd47@gmail.com>
2026-01-11 22:05:02 +0900

    Add Task Manager Model

---
+++ tasks/task_manager.py
@@ -0,0 +1,24 @@
+from utils.file_handler import load_data, save_data
+from tasks.task import Task
+
+TASK_FILE = 'data/tasks.json'
+
+class TaskManager:
+    def add_task(self, title):
+        task = Task(title)
+        tasks = load_data(TASK_FILE)
+        tasks.append(task.to_dict())
+        save_data(TASK_FILE, tasks)
+
+    def get_all_tasks(self):
+        return load_data(TASK_FILE)
+
+    def get_pending_tasks(self):
+        tasks = load_data(TASK_FILE)
+        return [task for task in tasks if not task['completed']]
+
+    def mark_task_completed(self, index):
+        tasks = load_data(TASK_FILE)
+        if 0 <= index < len(tasks):
+            tasks[index]['completed'] = True
+            save_data(TASK_FILE, tasks)
